033a7a440ae15d10ee253efbe3d7f6f9edf9cac4
aaron-zhao15 <aaron.zhao15@gmail.com>
2019-02-28 20:30:17 -0800

    update SmileyFace and BigHouse

--- src/GraphicsUnit1/BigHouse.java
+++ src/GraphicsUnit1/BigHouse.java
@@ -41,6 +41,33 @@ public class BigHouse extends Canvas
       int[] xArray = {400, 150, 650};
       int[] yArray = {50, 200, 200};
       window.fillPolygon(xArray, yArray, 3);
+      
+      window.setColor(Color.WHITE);
+      window.fillRect(350, 450, 100, 150);
+      
+      window.setColor(Color.YELLOW);
+      window.fillRect(300, 250, 50, 50);
+      window.fillRect(500, 250, 50, 50);
+      
+      window.setColor(Color.DARK_GRAY);
+      window.fillRect(50, 400, 50, 200);
+      window.fillRect(700, 400, 50, 200);
+      
+      window.setColor(Color.GREEN);
+      window.fillOval(5, 275, 150, 150);
+      window.fillOval(650, 275, 150, 150);
+      
+      window.setColor(Color.BLACK);
+      window.fillRect(250, 550, 25, 50);
+      
+      window.setColor(Color.GRAY);
+      int[] newXArray = {125, 150, 400, 375};
+      int[] newYArray = {580, 600, 520, 500};
+      window.fillPolygon(newXArray, newYArray, 4);
+      
+      window.setColor(Color.RED);
+      window.drawString("See-Saw", 250, 500);
+      
 
    }
 }
