70886b380b1456bb376b4f58d786eb4e964feb1b
aaron-zhao15 <aaron.zhao15@gmail.com>
2019-02-28 22:59:23 -0800

    update Robot

--- src/GraphicsUnit1/Robot.java
+++ src/GraphicsUnit1/Robot.java
@@ -17,7 +17,7 @@ class Robot extends Canvas
    public Robot()    //constructor method - sets up the class
    {
       setSize(800,600);
-      setBackground(Color.WHITE);   	
+      setBackground(Color.DARK_GRAY);   	
       setVisible(true);
    }
 
@@ -28,9 +28,12 @@ class Robot extends Canvas
       window.drawString("Robot LAB ", 35, 35 );
 
       //call head method
+      head(window);
       
       //call other methods
+      upperBody(window);
       
+      lowerBody(window);
    }
 
    public void head( Graphics window )
@@ -39,21 +42,34 @@ class Robot extends Canvas
 
       window.fillOval(300, 100, 200, 100);
 
+      window.setColor(Color.BLACK);
+      window.fillOval(350, 120, 20, 20);
+      window.fillOval(450, 120, 20, 20);
+      window.fillOval(390, 150, 10, 10);
 
-		//add more code here
+      window.setColor(Color.BLUE);
+      window.drawArc(300, 80, 200, 100, 240, 60);
+
 				
    }
 
    public void upperBody( Graphics window )
    {
-
-		//add more code here
+       window.setColor(Color.RED);
+       window.fillRect(325, 250, 150, 100);
+       
+       window.setColor(Color.BLACK);
+       window.drawLine(325, 250, 250, 325);
+       window.drawLine(475, 250, 550, 325);
    }
 
    public void lowerBody( Graphics window )
    {
-
-		//add more code here
-
+       window.setColor(Color.WHITE);
+       window.fillRect(325, 400, 150, 100);
+       
+       window.setColor(Color.BLACK);
+       window.drawLine(325, 500, 250, 575);
+       window.drawLine(475, 500, 550, 575);
    }
 }
